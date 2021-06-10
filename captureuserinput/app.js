616261821becd90ddd407e11a239902bba2a27df
Abdul Rehman Khalil <abdulrehmankhalil@abduls-air.home>
2021-06-10 09:18:47 +0100

    inquirer done

--- captureuserinput/app.js
+++ captureuserinput/app.js
@@ -1,14 +1,26 @@
-const fs = require(`fs`);
-const generatePage = require(`./src/page-template`);
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, gitHub] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+inquirer 
+    .prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name',
+
+        }
+
+    ])
+    .then (answers => console.log(answers));
+
+// const pageHTML = generatePage(name,gitHub);
 
 
+// fs.writeFile('index.html', generatePage(name, gitHub), err => {
 
-fs.writeFile('index.html', generatePage(name, gitHub), err => {
-
-    if (err) {
-        throw err;
-    }
-    console.log("portfolio complete");
-})
+//     if (err) {
+//         throw err;
+//     }
+//     console.log("portfolio complete");
+// })
